2a539d727f5f99340e9474652024bcd91f182e7f
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 15:40:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,11 +7,11 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num1 * num2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  num1 / num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
@@ -23,5 +23,5 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  num1 + (( num2 * num3 ) / num4 )
+  num1 + (( num2 * num3 ) / num4 )
 end
